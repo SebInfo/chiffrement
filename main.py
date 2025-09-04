7c5f0b5ed37d560267af7ae6c4a54a6c8e667799
SebInfo <contact@inion.info>
2025-09-04 09:30:07 +0200

    Projet chiffrement

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import time
 import string
 from itertools import permutations
+import unidecode
 
-def chiffrement(message,dec):
+def chiffrementCesar(message,dec):
+    dec=dec%25
     caracteresNonTraite=[' ',"'",'!','.']
     message=message.lower()
     resultat=""
@@ -16,7 +18,7 @@ def chiffrement(message,dec):
         resultat=resultat+chr(x)
     return resultat
 
-def dechiffrement(message,dec):
+def dechiffrement_cesar(message,dec):
     caracteresNonTraite=[' ',"'",'!','.']
     message=message.lower()
     retour=""
@@ -34,11 +36,13 @@ def chiffrement_vigenere(mot,cle):
     mot_code = ""
     # i récupère l'indice c le caractère du mot
     for i,c in enumerate(mot):
-        # d va récupérer le caractère de la cle et boucler
+        # d va récupérer le caractère de la cle (mot de passe) et boucle
         d = cle[i % len(cle)]
         # On ramène la lettre A à l'indice 0 (ord('A')=65)
+
         # d va être le décalage
         d = ord(d) - 65
+
         # On ramène aussi le caractère c sur le même principe
         c = ord(c) - 65
         # On va calculer le nouveau caractère pour chiffrer
@@ -48,7 +52,7 @@ def chiffrement_vigenere(mot,cle):
         mot_code += nouveauCaractere
     return mot_code
 
-def dechiffrement_vigenere(mot,cle ):
+def dechiffrement_vigenere(mot,cle):
     mot_code = ""
     for i,c in enumerate(mot):
         d = cle[ i % len(cle)]
@@ -70,8 +74,31 @@ def attaque_dict(mot):
         dechif=dechiffrement_vigenere(mot,clef)
         if dechif+"\n" in lignes:
             end = time.time()
-            print("Mot de passe KC \nMot trouvé c'était :"+dechif+"\nLe mot de passe utlisé était "+clef)
+            print(lignes.__sizeof__())
+            print("Mot de passe KC \nLe mot à trouver c'était :"+dechif+"\nLe mot de passe utlisé était "+clef)
             print("\nen ",end-start, " secondes")
+
+
+def chiffrement_substitution(texte, cle_substitution):
+    texte_chiffre = ""
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    for caractere in texte:
+        if caractere.upper() in alphabet:
+            # Trouver l'indice de la lettre dans l'alphabet
+            index = alphabet.index(caractere.upper())
+            # Appliquer la substitution en fonction de la clé
+            nouveau_caractere = cle_substitution[index]
+
+            # Conserver la casse (majuscule ou minuscule)
+            if caractere.islower():
+                texte_chiffre += nouveau_caractere.lower()
+            else:
+                texte_chiffre += nouveau_caractere
+        else:
+            texte_chiffre += caractere  # Conserver la ponctuation et les espaces
+
+    return texte_chiffre
 
 def attaque_brute(mot):
     print("Début du déchiffrement brute pour le mot "+mot)
@@ -86,7 +113,7 @@ def attaque_brute(mot):
     mot=mot.upper()
     L=list(string.ascii_uppercase)
     # combinaisons sur 4 lettres
-    combinaisons =  permutations(L,4)
+    combinaisons =  permutations(L,5)
     lignes =[''.join(j) for j in combinaisons]
     for clef in lignes:
         comp+=1
@@ -102,15 +129,73 @@ def attaque_brute(mot):
             return 1
     return 0
 
-#cryp=chiffrement("C'est lundi et tout le monde est heureux de faire du python !",2)
+result=chiffrementCesar("Bonjour !",7)
+print(result)
+
+print(dechiffrement_cesar(result,7))
+
+#Cesar2=chiffrementCesar("C'est Mardi et tout le monde est heureux de faire du Python en bloc 3 !",2)
+#print(Cesar2)
 #print(cryp)
 #for dec in range(1,26):
 #    claire=dechiffrement(cryp,dec)
 #    print(claire)
-chiff=chiffrement_vigenere("VENDREDI","GFKU")
-print(chiff)
-#dechiff=dechiffrement_vigenere(chiff,"BTS_SIO")
+#chiff=chiffrement_vigenere("VENDREDI","B")
+#print(chiff)
+#dechiff=dechiffrement_vigenere(chiff,"B")
 #print(dechiff)
 #attaque_dict("SCNYILAECHF")
 #attaque_dict("XLRYRPFP")
-attaque_brute("BBJXXXJNC")
+#attaque_brute("BJXXQKIS")
+#print(Cesar2);
+
+#print (dechiffrement_cesar("ivuqvby !", 7))
+#print (dechiffrement_cesar("e'guv octfk gv vqwv ng oqpfg guv jgwtgwz fg hcktg fw ravjqp gp dnqe 5 !",2))
+
+
+
+def normaliser_texte(texte):
+    # Supprimer les accents et convertir en majuscules
+    texte_normalise = unidecode.unidecode(texte).upper()
+    return texte_normalise
+
+texte = """
+À m'asseoir sur un banc, cinq minutes, avec toi
+Et regarder les gens, tant qu'y en a
+Te parler du bon temps, qui est mort ou qui reviendra
+En serrant dans ma main tes petits doigts
+Pis donner à bouffer à des pigeons idiots
+Leur filer des coups de pied pour de faux
+Et entendre ton rire qui lézarde les murs
+Qui sait surtout guérir mes blessures
+Te raconter un peu comment j'étais, minot
+Les bombecs fabuleux qu'on piquait chez l'marchand
+Car-en-sac et Minto, caramels à un franc
+Et les Mistral Gagnants
+À remarcher sous la pluie, cinq minutes, avec toi
+Et regarder la vie, tant qu'y en a
+Te raconter la Terre en te bouffant des yeux
+Te parler de ta mère, un petit peu
+Et sauter dans les flaques pour la faire râler
+Bousiller nos godasses et s'marrer
+Et entendre ton rire comme on entend la mer
+S'arrêter, repartir en arrière
+Te raconter surtout les Carambars d'antan et les Coco Boers
+Et les vrais Roudoudous qui nous coupaient les lèvres
+Et nous niquaient les dents
+Et les Mistral Gagnants
+À m'asseoir sur un banc, cinq minutes, avec toi
+Regarder le soleil qui s'en va
+Te parler du bon temps, qui est mort et je m'en fous
+Te dire que les méchants, c'est pas nous
+Que si moi je suis barge, ce n'est que de tes yeux
+Car ils ont l'avantage d'être deux
+Et entendre ton rire s'envoler aussi haut
+Que s'envolent les cris des oiseaux
+Te raconter, enfin, qu'il faut aimer la vie
+L'aimer même si le temps est assassin et emporte avec lui
+Les rires des enfants
+Et les Mistral Gagnants
+Et les Mistral Gagnants"""
+#cle_substitution = "ZXVQRWYTPNMKJBFGLHDOAISCUE"
+#print(chiffrement_substitution(normaliser_texte(texte), cle_substitution))
